f99f4dc23156f886e28b9c5fbff24c283a14adc3
TNTEMPLET <68797176+TNTEMPLET@users.noreply.github.com>
2022-12-09 14:40:49 -0600

    initial

---
+++ Day 3/rucksack.py
@@ -0,0 +1,11 @@
+f = open('/Users/trent.templet/Dev/WebApps/ZTM/Advent-of-Code-2022/Day 3/input.txt', 'r')
+input_raw = f.read()
+
+def compartment_reallocate(data):
+    priority = 0
+
+    for x in data:
+        charnum = len(x)
+        print(charnum)
+
+    
